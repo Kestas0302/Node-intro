9ea3b7033a6343b4058a8ccb17e97f8e78571bf3
Kestas0302 <kestutis.margelis1992@gmail.com>
2022-08-01 17:50:40 +0300

    some node examples

---
+++ 1paskaita/index.js
@@ -0,0 +1,13 @@
+var casual = require("casual");
+
+console.log(casual.city);
+// console.log("Kestutis Margelis");
+
+// random skaiciaus funkcija pasirasyti, kurioje galime irasyti nuo kiek iki kiek random skaicius ir ji grazinti
+// function(1, 10) => 5
+// function(2, 7) => 4
+
+// sukonstruoti user objektą iš casual random properčių
+
+// turi turėti vardą, pavardę, lytį, adresas (objektas su šalis, miestas, gatvė), el. paštas, slaptažodis, metai (0-99), kuri mėn gimė., mėgstamiausia spalva
+// Įsirašyti package pavadinimu 'uuid' versiją 7.0.2
---
+++ 2paskaita/server.js
@@ -0,0 +1,39 @@
+const http = require("http");
+const hostname = "127.0.0.1";
+const port = 5500;
+const users = [
+  { name: "Rokas", password: "rokas123" },
+  { name: "Tomas", password: "tomas123" },
+];
+
+const movies = [
+  { name: "Nemo", hours: "2" },
+  { name: "Avatar", hours: "3" },
+  { name: "Cars",phours: "2" },
+
+];
+
+const server = http.createServer((req, res) => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+  };
+  res.writeHead(200, headers);
+
+  switch (req.url) {
+    case "/users":
+      res.write(JSON.stringify(users));
+      res.end();
+      break;
+      case "/movies":
+        res.write(JSON.stringify(movies));
+        res.end();
+        break;
+    default:
+      res.write(JSON.stringify([]));
+      res.end();
+  }
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running on port: ${port}`);
+});
